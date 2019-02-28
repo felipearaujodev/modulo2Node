ead1abf438d40dc9ac396256827fb736fceff49c
Felipe Araujo <comunicar.felipearaujo@gmail.com>
2019-02-28 20:55:21 -0300

    Criando Agendamentos

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,8 @@ const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 const DashboardController = require('./app/controllers/DashboardController')
 const FileController = require('./app/controllers/FileController')
+const AppointmentsController = require('./app/controllers/AppointmentsController')
+const AvailableController = require('./app/controllers/AvailableController')
 
 routes.use((req, res, next) => {
   res.locals.flashSucces = req.flash('success')
@@ -34,5 +36,9 @@ routes.post('/signup', upload.single('avatar'), UserController.store)
 routes.get('/app/logout', SessionController.destroy)
 
 routes.get('/app/dashboard', DashboardController.index)
+
+routes.get('/app/appointments/new/:provider', AppointmentsController.create)
+
+routes.get('/app/available/:provider', AvailableController.index)
 
 module.exports = routes
